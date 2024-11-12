d5ea24a74dc0bbd378ccb32b56ff2fd7fd4d552e
serge-starynets <sergii.starynets@gmail.com>
2024-11-12 15:06:23 +0200

    add circleshape class

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
